95bdfaaceed64099796f37bc390bb2ed9940481a
vinu <vinukrishnan3694>
2017-09-11 12:40:03 +0530

    download-unzip-sort csv

---
+++ unzip_file.rb
@@ -0,0 +1,35 @@
+require 'csv'
+require 'rubygems'
+require 'zip'
+require 'net/http'
+
+class UnzipFile
+  def unzip_file (file, destination)
+    Zip::File.open(file) do |zip_file|
+      zip_file.each do |f|
+        f_path = File.join(destination, f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        f.extract(f_path)unless File.exist?(f_path)
+        @a=f
+      end
+    end    
+    a="#{@a}"
+    i=1
+    $ar=[]
+    CSV.foreach(a) do |row| 
+      $ar << row[1]      
+      i=i+1
+      if (i==1000)         
+        break
+      end
+    end
+  end
+  puts"Unzip Completed"
+end
+
+@unzip=UnzipFile.new
+@unzip.unzip_file("top","/home/vinu/projects/ruby")
+
+
+
+
